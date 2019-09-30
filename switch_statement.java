52c967f42e1631f952f2cc1a2cf4721d00503338
Sebastian Weiss <sebastian.weiss2@ikea.com>
2019-09-30 11:27:52 +0200

    Create switch_statement.java

---
+++ switch_statement.java
@@ -0,0 +1,22 @@
+class apples{
+	public static void main(String args[]){
+		int age;
+		age = 3;
+
+		switch (age){
+			case 1:
+				System.out.println("You can crawl");
+				break;
+			case 2:
+				System.out.println("You can talk");
+				break;
+			case 3:
+				System.out.println("You can get in trouble");
+				break;
+			default:
+				System.out.println("I dont know how old you are");
+			break;
+			
+		}
+	}
+}
